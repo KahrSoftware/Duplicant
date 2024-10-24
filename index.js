6a0f1367cca9ebb69614b89015ee1728bf9f37b4
KahrSoftware <justin@kahr.org>
2024-10-23 22:54:50 -0500

    Core reaction functionality
    The discord bot reacts to every message sent by a user in a specific
    channel.

--- index.js
+++ index.js
@@ -4,10 +4,20 @@ const config = require('./config.json');
 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+] });
+
+client.on(Events.MessageCreate, message => {
+	if(message.channelId == config.channel && message.author.id == config.user)
+	{
+		message.react(config.reaction);
+	}
+})
 
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`${readyClient.user.tag} started`);
 });
 
 client.login(env.parsed.TOKEN);
