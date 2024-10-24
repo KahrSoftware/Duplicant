696801d224393a8e6c781d27105f0d95ede2fd9f
KahrSoftware <justin@kahr.org>
2024-10-23 20:37:53 -0500

    Basic node discord application setup

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const env = require('dotenv').config();
+const config = require('./config.json');
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(env.parsed.TOKEN);
